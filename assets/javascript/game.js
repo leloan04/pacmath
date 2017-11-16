18cf58a56e2821a024a48d74c53f34e37aaf2aca
Loan Le <32397554+leloan04@users.noreply.github.com>
2017-11-16 18:43:12 -0500

    psuedocodes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,8 +11,26 @@ $(document).ready(function() {
 
     var counter=0;
 
-    var ghostNumbers = [20, 9, 55, 1];
+    var ghostNumber = [20, 9, 55, 1];
 
+// Assign numbers in array to each image for whenever clicked
+// Each image is used to add to get to the targetNumber
+
+$(".ghost").on("click", function(){
+
+$("#blinky").attr("data-ghostValue", ghostNumber[0]);
+
+$("#pinky").attr("data-ghostValue", ghostNumber[1]);
+
+$("#inky").attr("data-ghostValue", ghostNumber[2]);
+
+$("#clyde").attr("data-ghostValue", ghostNumber[3]);
+
+// If targetNumber is reached, user wins, if loses, user loses
+// a life
+
+// Reset game once all lives are lost with RESET button
+})
 
 
 
