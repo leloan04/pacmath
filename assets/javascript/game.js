06a151bffe62a211e1eff595849e63fbb7ebba16
Loan Le <32397554+leloan04@users.noreply.github.com>
2017-11-16 14:29:37 -0500

    updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    var minNumber = 1;
+    var maxNumber = 100;
+    var targetNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+
+    $("#targetNumber").append(" " + targetNumber);
+    
+    var wins=0;
+    var lives=3;
+
+    var counter=0;
+
+    var ghostNumbers = [20, 9, 55, 1];
+
+
+
+
+
+
+
+
+
+
+});   
